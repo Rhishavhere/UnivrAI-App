6c34c9214307a8872425c46c0932dd3ecab53230
Rhishavhere <rhishh.me@gmail.com>
2025-10-16 07:29:12 +0530

    layout updates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Menu } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Sidebar from './Sidebar';
+import GradientText  from './GradientText';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,9 +12,9 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gradient-bg">
+    <div className="min-h-screen bg-transparent">
       {/* Header */}
-        <div className='absolute flex justify-center items-center'>
+        <div className='absolute flex justify-center items-center z-10 mt-8 ml-4'>
           <Button
             variant="ghost"
             size="icon"
@@ -22,14 +23,24 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           >
             <Menu className="h-6 w-6" />
           </Button>
-          <p className='font-mono'>CMRIT</p>
+          <div className='flex gap-2 justify-center items-center' onClick={() => window.location.href = '/home'}>
+            <p className='font-sans font-medium text-sm'>CMRIT</p>
+            <GradientText
+              colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+              animationSpeed={3}
+              showBorder={false}
+              className="custom-class"
+            >
+              NEXT
+            </GradientText>
+          </div>
         </div>
 
       {/* Sidebar */}
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       {/* Main Content */}
-      <main className="pt-16 min-h-screen">
+      <main className=" min-h-screen">
         {children}
       </main>
     </div>
